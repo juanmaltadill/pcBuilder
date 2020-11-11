4923f4b34693f2af47d47fe46289663f51ba4c36
juanmaltadill <juanmaltadill@gmail.com>
2020-11-11 11:44:37 +0100

    base
    base del proyecto

---
+++ pcBuilder/pcBuilder/Componente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pcBuilder
+{
+    public class Componente
+    {
+        public string Tipo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Precio { get; set; }
+
+        public override string ToString()
+        {
+            return Tipo;
+        }
+    }
+}
--- pcBuilder/pcBuilder/MainPage.xaml.cs
+++ pcBuilder/pcBuilder/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,37 @@ namespace pcBuilder
 {
     public partial class MainPage : ContentPage
     {
+       public IList<Componente> Componentes { get; set; }
+
         public MainPage()
         {
+
             InitializeComponent();
+
+            Componentes = new List<Componente>();
+            Componentes.Add(new Componente
+            {
+                Tipo = "Tarjeta Gráfica",
+                Marca = "Nvidia",
+                Modelo = "GTX 1070",
+                Precio = 250,
+            });
+            Componentes.Add(new Componente
+            {
+                Tipo = "Tarjeta Gráfica",
+                Marca = "Nvidia",
+                Modelo = "GTX 1060",
+                Precio = 250,
+            });
+            Componentes.Add(new Componente
+            {
+                Tipo = "Tarjeta Gráfica",
+                Marca = "Nvidia",
+                Modelo = "GTX 1080",
+                Precio = 250,
+            });
+            BindingContext = this;
+            
         }
-    }
+    }  
 }
